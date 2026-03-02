89280f7a6a53bf34f4d2af340d5702821df835f9
Matheus-Leonardo <matheusct3@hotmail.com>
2026-03-02 18:04:10 -0300

    feat: implementando requisições com filtro por id

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
-	// Importando seus próprios pacotes internos
 	"api-estudo/internal/entities"
 	"api-estudo/internal/helpers"
 )
 
-// Simulação de banco de dados em memória
 var products = map[string]entities.Product{
 	"1": {ID: "1", Name: "Guitarra Fender", Type: "Instrumento", Quantity: 5},
 }
@@ -20,15 +19,37 @@ var products = map[string]entities.Product{
 func main() {
 	r := chi.NewRouter()
 
-	// Middlewares para monitorar a API no terminal
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	// Rota para listagem de produtos (GET)
 	r.Get("/products", func(w http.ResponseWriter, r *http.Request) {
 		helpers.WriteJsonResponse(w, http.StatusOK, products)
 	})
 
-	fmt.Println("API de Estoque Reiniciada na porta 8081...")
+	r.Get("/products/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id") // Extrai o ID da URL usando o recurso do Chi
+		product, ok := products[id] // O "idioma" do Go para verificar se a chave existe
+		if !ok {
+			helpers.WriteJsonResponse(w, http.StatusNotFound, map[string]string{"error": "produto não encontrado"})
+			return
+		}
+		helpers.WriteJsonResponse(w, http.StatusOK, product)
+	})
+
+	r.Post("/products", func(w http.ResponseWriter, r *http.Request) {
+		var newProduct entities.Product
+
+		err := json.NewDecoder(r.Body).Decode(&newProduct)
+		if err != nil {
+			helpers.WriteJsonResponse(w, http.StatusBadRequest, map[string]string{"error": "JSON inválido"})
+			return
+		}
+
+		products[newProduct.ID] = newProduct
+
+		helpers.WriteJsonResponse(w, http.StatusCreated, newProduct)
+	})
+
+	fmt.Println("API Completa (Ep. 1) rodando na porta 8081...")
 	http.ListenAndServe(":8081", r)
 }
