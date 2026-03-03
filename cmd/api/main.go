0df491126615a1e0833cbc511546d651f482d6eb
Matheus-Leonardo <matheusct3@hotmail.com>
2026-03-03 17:48:58 -0300

    feat: dockerização da API com implementação do gitignore

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -50,6 +50,39 @@ func main() {
 		helpers.WriteJsonResponse(w, http.StatusCreated, newProduct)
 	})
 
+	r.Put("/products/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+
+		// Verificamos se o item existe antes de tentar mudar
+		if _, ok := products[id]; !ok {
+			helpers.WriteJsonResponse(w, http.StatusNotFound, map[string]string{"error": "produto inexistente"})
+			return
+		}
+
+		var updatedProduct entities.Product
+		if err := json.NewDecoder(r.Body).Decode(&updatedProduct); err != nil {
+			fmt.Println("Erro ao decodificar JSON do PUT:", err) // Log para você ver no terminal
+			helpers.WriteJsonResponse(w, http.StatusBadRequest, map[string]string{"error": "JSON inválido"})
+			return
+		}
+
+		updatedProduct.ID = id        // Forçamos o ID da URL para garantir consistência
+		products[id] = updatedProduct // Atualizamos a "gaveta" correta do mapa
+
+		fmt.Printf("Produto %s atualizado com sucesso!\n", id)
+		helpers.WriteJsonResponse(w, http.StatusOK, updatedProduct)
+	})
+
+	r.Delete("/products/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+		if _, ok := products[id]; !ok {
+			helpers.WriteJsonResponse(w, http.StatusNotFound, map[string]string{"error": "produto não encontrado"})
+			return
+		}
+		delete(products, id)                // Função nativa do Go para remover de mapas
+		w.WriteHeader(http.StatusNoContent) // 204: Sucesso, mas sem corpo de resposta
+	})
+
 	fmt.Println("API Completa (Ep. 1) rodando na porta 8081...")
 	http.ListenAndServe(":8081", r)
 }
