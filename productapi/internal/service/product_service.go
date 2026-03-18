1769ec525a76dc8bab32c0f0a917c0ac99657e6d
Matheus-Leonardo <matheusct3@hotmail.com>
2026-03-17 21:18:57 -0300

    teste

--- productapi/internal/service/product_service.go
+++ productapi/internal/service/product_service.go
@@ -7,21 +7,27 @@ import (
 	"strings"
 )
 
+// ProductService define o que o negócio espera de um produto
 type ProductService interface {
 	Create(p entities.Product) error
 	GetAll() []entities.Product
+	GetByID(id string) (entities.Product, error) // retorna product + error
+	Update(id string, p entities.Product) error
+	Delete(id string) error
 }
 
 type productService struct {
+	// Cross-check: Depende da interface do repositório, não da implementação MySQL
 	repo repository.ProductRepository
 }
 
+// NewProductService injeta o repositório no serviço
 func NewProductService(repo repository.ProductRepository) ProductService {
 	return &productService{repo: repo}
 }
 
 func (s *productService) Create(p entities.Product) error {
-
+	// Remove espaços em branco antes de validar
 	if strings.TrimSpace(p.Name) == "" {
 		return errors.New("O nome do produto é obrigatório")
 	}
@@ -30,4 +36,22 @@ func (s *productService) Create(p entities.Product) error {
 
 func (s *productService) GetAll() []entities.Product {
 	return s.repo.GetAll()
+}
+func (s *productService) GetByID(id string) (entities.Product, error) {
+	product, ok := s.repo.GetByID(id)
+	if !ok {
+		return entities.Product{}, errors.New("produto não encontrado")
+	}
+	return product, nil
+}
+
+func (s *productService) Update(id string, p entities.Product) error {
+	if strings.TrimSpace(p.Name) == "" {
+		return errors.New("O nome do produto é obrigatório")
+	}
+	return s.repo.Update(id, p)
+}
+
+func (s *productService) Delete(id string) error {
+	return s.repo.Delete(id)
 }
